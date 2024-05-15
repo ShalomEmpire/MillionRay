66e5095f72723781402f3f03dcfd6fff873d25c2
Davi Ribeiro <davi.rb@hotmail.com>
2024-05-14 22:10:15 -0300

    creating navigation routes.

---
+++ app/(drawer)/setings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Setings() {
+  return (
+    <View>
+      <Text>setings</Text>
+    </View>
+  );
+}
